b8c3b4d63378c2e764bf94895da5ec982993dfd0
Vlad Skrynyk <skrynyk.vlad@gmail.com>
2023-08-03 12:21:56 +0300

    -

--- frontend/src/features/getRoute/getRouteData.js
+++ frontend/src/features/getRoute/getRouteData.js
@@ -19,8 +19,8 @@ export const getRouteInfo = createAsyncThunk("route/getRouteInfo", async ({ from
 })
 
 export const getRouteInfoDetail = createAsyncThunk("route/getRouteInfoDetail", async ({ id, id_from, id_to }) => {
-     console.log("tytt", `${BASE_URL}/get_route_info?route_id=${id}&move_from=${id_from}&move_to=${id_to}`, id);
-     const response = await axios.get(`${BASE_URL}/get_route_info?route_id=${id}`, 
+     console.log("tytt", `${BASE_URL}/get_path_info?route_id=${id}&move_from=${id_from}&move_to=${id_to}`, id);
+     const response = await axios.get(`${BASE_URL}/get_path_info?route_id=${id}&move_from=${id_from}&move_to=${id_to}`, 
      {
           withCredentials: false
      })
