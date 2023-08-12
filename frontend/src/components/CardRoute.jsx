f9fcd8a4db9e0ca4b2d7db98686ea2e53829a245
Vlad Skrynyk <skrynyk.vlad@gmail.com>
2023-08-12 12:39:06 +0300

    fix bug

--- frontend/src/components/CardRoute.jsx
+++ frontend/src/components/CardRoute.jsx
@@ -31,7 +31,7 @@ const CardRoute = ({ route_id, move_from , move_to }) => {
           <div>
                <div class="card new_card" >
                     <div class="card-body">
-                         <p class="glav_data">{weeks[parsed_time_from_pre.getDay()]},{parsed_time_from_pre.getDate()} {months[parsed_time_from_pre.getMonth()].toLocaleLowerCase()},{parsed_time_from}</p>
+                         <p class="glav_data">{weeks[parsed_time_from_pre.getDay()]},{parsed_time_from_pre.getDate()} {months[parsed_time_from_pre.getMonth()]?.toLocaleLowerCase()},{parsed_time_from}</p>
                          <ul class="event ">
                               <li>
                                    <time >{parsed_time_from}</time>
