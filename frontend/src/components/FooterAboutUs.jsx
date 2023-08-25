f8dfb5135926e7b2a412ac8177f138006c14c3a7
gaponukz <gaponukz54@gmail.com>
2023-08-25 18:23:07 +0300

    Add some navigate

--- frontend/src/components/FooterAboutUs.jsx
+++ frontend/src/components/FooterAboutUs.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 const FooterAboutUs = ({ check }) => {
+  const navigate = useNavigate();
   return (
     <div>
      <div className="container-1 ersttts" >
@@ -11,17 +13,17 @@ const FooterAboutUs = ({ check }) => {
             
             <div className="row" style={{ paddingBottom: " 50px",}} id="card">
                <div className="col" >
-                  <div className="card next_card">
+                  <div className="card next_card" onClick={()=>{navigate("/route?move_from_city=Київ&move_to_city=Варшава&date=*")}}>
                      <div className="card-body route_card_body">
                         <div className="sq">
-                           Київ
+                           Київ {'  '}
                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="kirk-icon sc-ktwOSD kPQuM sc-dGAOeH GAPhQ" width="24" height="24" aria-hidden="true">
                               <g fill="none" stroke="#708C91" strokeWidth="2" strokeMiterlimit="10" strokeLinejoin="round" strokeLinecap="round" transform="rotate(180 12 12)">
                                  <path d="M9 19l-7-7 7-7"></path>
                                  <path d="M22 12H2"></path>
                               </g>
                            </svg>
-                           Варшава
+                           {'  '} Варшава
                         </div>
                         <div >
                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="kirk-icon2 sc-ktwOSD kPQuM" width="24" height="24" aria-hidden="true">
@@ -32,17 +34,17 @@ const FooterAboutUs = ({ check }) => {
                   </div>
                </div>
                <div className="col">
-                  <div className="card next_card">
+                  <div className="card next_card" onClick={()=>{navigate("/route?move_from_city=Бориспіль&move_to_city=Лодзь&date=*")}}>
                      <div className="card-body route_card_body">
                         <div >
-                           Варшава
+                           Бориспіль {'  '}
                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="kirk-icon sc-ktwOSD kPQuM sc-dGAOeH GAPhQ" width="24" height="24" aria-hidden="true">
                               <g fill="none" stroke="#708C91" strokeWidth="2" strokeMiterlimit="10" strokeLinejoin="round" strokeLinecap="round" transform="rotate(180 12 12)">
                                  <path d="M9 19l-7-7 7-7"></path>
                                  <path d="M22 12H2"></path>
                               </g>
                            </svg>
-                           Київ
+                           {'  '} Лодзь
                         </div>
                         <div >
                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="kirk-icon2 sc-ktwOSD kPQuM" width="24" height="24" aria-hidden="true">
@@ -53,17 +55,17 @@ const FooterAboutUs = ({ check }) => {
                   </div>
                </div>
                <div className="col">
-                  <div className="card next_card">
+                  <div className="card next_card" onClick={()=>{navigate("/route?move_from_city=Київ&move_to_city=Познань&date=*")}}>
                      <div className="card-body route_card_body">
                         <div >
-                           Київ
+                           Київ {'  '}
                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="kirk-icon sc-ktwOSD kPQuM sc-dGAOeH GAPhQ" width="24" height="24" aria-hidden="true">
                               <g fill="none" stroke="#708C91" strokeWidth="2" strokliterlimit="10" strokeLinejoin="round" strokeLinecap="round" transform="rotate(180 12 12)">
                                  <path d="M9 19l-7-7 7-7"></path>
                                  <path d="M22 12H2"></path>
                               </g>
                            </svg>
-                           Львів
+                           {'  '} Познань
                         </div>
                         <div >
                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="kirk-icon2 sc-ktwOSD kPQuM" width="24" height="24" aria-hidden="true">
